7f89cad1a3106328f608fe0523c40eeeede28f5c
Geoff Winner & Jill Kuchman <student@epicodus.com>
2015-03-02 15:47:41 -0800

    Add empty results message from empty array.

--- cartest.php
+++ cartest.php
@@ -5,17 +5,23 @@ class Car
     private $make_model;
     private $price;
     private $miles;
+    private $image_path;
 
-    function __construct($name, $cost, $mileage)
+    function __construct($name, $cost, $mileage, $image)
     {
         $this->make_model = $name;
         $this->price = $cost;
         $this->miles = $mileage;
+        $this->image_path = $image;
     }
 
     function getMakeModel()
     {
         return $this->make_model;
+    }
+    function getPrice()
+    {
+        return $this->price;
     }
 
     function setMiles($new_miles)
@@ -29,19 +35,19 @@ class Car
     {
             return $this->miles;
     }
-    function getPrice()
+    function getImage()
     {
-        return $this->price;
+        return $this->image_path;
     }
 }
 
-$porsche = new Car("2014 Porsche 911", 114991, 7804);
+$porsche = new Car("2014 Porsche 911", 114991, 7804, "porsche911.jpg");
 
-$ford = new Car("2011 Ford F450", 55995, 14241);
+$ford = new Car("2011 Ford F450", 55995, 14241, "f450.jpg");
 
-$lexus = new Car("2013 Lexus RX 350", 39900, 23000);
+$lexus = new Car("2013 Lexus RX 350", 39900, 23000, "lexusrx350.jpg");
 
-$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979,"cls550.jpg");
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
 
@@ -66,15 +72,23 @@ foreach ($cars as $car) {
     <h1>Your Car Dealership</h1>
     <ul>
             <?php
-                foreach ($cars_matching_search as $car) {
+                if (empty ($cars_matching_search)){
+                    echo "There are no results to display.";
+                }   else {
+
+                    foreach ($cars_matching_search as $car)
+                    {
                     $car_price = $car->getPrice();
                     $car_make_model = $car->getMakeModel();
                     $car_miles = $car->getMiles();
+                    $car_image = $car->getImage();
                     echo "<li> $car_make_model </li>";
+                    echo "<img src='$car_image'>";
                     echo "<ul>";
                         echo "<li> $$car_price </li>";
                         echo "<li> Miles: $car_miles </li>";
                     echo "</ul>";
+                    }
                 }
             ?>
     </ul>
