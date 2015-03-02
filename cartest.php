574ac99b9e387c45224c2499b1efafb1ce8d84d4
Geoff Winner & Jill Kuchman <student@epicodus.com>
2015-03-02 15:12:07 -0800

    calculate price/miles with logical operators

--- cartest.php
+++ cartest.php
@@ -2,8 +2,8 @@
 
 class Car
 {
-    public $make_model;
-    public $price;
+    private $make_model;
+    private $price;
     private $miles;
 
     function __construct($name, $cost, $mileage)
@@ -11,6 +11,11 @@ class Car
         $this->make_model = $name;
         $this->price = $cost;
         $this->miles = $mileage;
+    }
+
+    function getMakeModel()
+    {
+        return $this->make_model;
     }
 
     function setMiles($new_miles)
@@ -24,13 +29,17 @@ class Car
     {
             return $this->miles;
     }
+    function getPrice()
+    {
+        return $this->price;
+    }
 }
 
 $porsche = new Car("2014 Porsche 911", 114991, 7804);
 
 $ford = new Car("2011 Ford F450", 55995, 14241);
 
-$lexus = new Car("2013 Lexus RX 350", 78000, 23000);
+$lexus = new Car("2013 Lexus RX 350", 39900, 23000);
 
 $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
@@ -38,7 +47,10 @@ $cars = array($porsche, $ford, $lexus, $mercedes);
 
 $cars_matching_search = array();
 foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
+    $car_price = $car->getPrice();
+    $car_miles = $car->getMiles();
+    if ($car_price < $_GET["price"] || $car_miles < $_GET["miles"])
+    {
         array_push($cars_matching_search, $car);
     }
 }
@@ -55,10 +67,12 @@ foreach ($cars as $car) {
     <ul>
             <?php
                 foreach ($cars_matching_search as $car) {
+                    $car_price = $car->getPrice();
+                    $car_make_model = $car->getMakeModel();
                     $car_miles = $car->getMiles();
-                    echo "<li> $car->make_model </li>";
+                    echo "<li> $car_make_model </li>";
                     echo "<ul>";
-                        echo "<li> $$car->price </li>";
+                        echo "<li> $$car_price </li>";
                         echo "<li> Miles: $car_miles </li>";
                     echo "</ul>";
                 }
